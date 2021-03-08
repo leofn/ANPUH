f7faf93f8c026f332c86a8994ae61974cbcd1223
ericbrasiln <ericbrasiln@protonmail.com>
2021-03-08 16:16:26 -0300

    arquivo para script de gráfico geral com todas as buscas

---
+++ R/GERAL.R
@@ -0,0 +1,19 @@
+# script para gerar gráfico com todos as variáveis em um único plot
+library(dplyr)
+library(tidyverse)
+library(lubridate)
+library(quanteda)
+library(stringi)
+library(stm)
+library(quanteda)
+library(rlist)
+library(anytime)
+require(tidytext)
+
+# Restaure o objeto df com a base de dados
+
+df <- readRDS(file = "data/df.rds")
+
+
+
+
